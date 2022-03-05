8527129b3961c0838e2e4a78724a1fd7da857e73
edgarbp95 <edgarbp95@hotmail.com>
2022-03-05 17:09:09 -0300

    act

---
+++ LANDING NUCBA 2/app.js
@@ -0,0 +1,91 @@
+
+const nombre = document.querySelector(".input-nombre");
+const apellido = document.querySelector(".input-apellido");
+const telefono = document.querySelector(".input-telefono");
+const email = document.querySelector(".input-email");
+const mensaje = document.querySelector(".input-mensaje");
+const estadoMensaje = document.querySelector(".estado-mensaje");
+const botonEnviar = document.querySelector(".boton-enviar-formulario");
+
+
+
+botonEnviar.addEventListener("click", (e)=>{
+
+let confirmar = confirm("¿Estás seguro que deseas enviar el mensaje?");
+
+if(confirmar){
+    if(nombre.value.length<3 || nombre.value.length>40){
+        estadoMensaje.innerHTML = "El nombre es invalido";
+        e.preventDefault();
+    }else if(telefono.value.length!=10){
+        estadoMensaje.innerHTML = "El numero de telefono que ingreso esta incompleto. Ingrese un número sin espacios ni guiones."
+        e.preventDefault();
+    }
+    
+}
+
+});
+
+const cambiarLetrasOscuras = ()=>{
+let elementos = document.querySelectorAll('.oscuro');
+let index = 0, length = elementos.length;
+for( ;index<length;index++){
+    elementos[index].classList.add('color_letras_oscuras');
+}
+}
+cambiarLetrasOscuras();
+
+const cambiarLetrasClaras = ()=>{
+    let elementos = document.querySelectorAll('.claro');
+    let index = 0, length = elementos.length;
+    for( ;index<length;index++){
+        elementos[index].classList.add('color_letras_claras');
+    }
+    }
+cambiarLetrasClaras();
+
+const rescatados = document.getElementById("rescatados");
+
+function disableScroll(){  
+    window.scrollTo(0, 0);
+  
+  
+  window.addEventListener('scroll', disableScroll);
+}
+
+function enableScroll(){
+  window.removeEventListener('scroll', disableScroll); 
+}
+
+rescatados.addEventListener("click",()=>{
+
+    let modal = document.querySelector(".modal_nuestros_rescatados");
+
+    modal.style.display = "flex";
+    
+    disableScroll();
+   
+})
+
+let cerrarModalRescatados = document.querySelector(".fa_fa_x");
+
+cerrarModalRescatados.addEventListener("click",()=>{
+    modal.style.display = "none";
+    enableScroll();
+})
+
+const textos = document.querySelectorAll(".texto_rescatado");
+const imagenes = document.querySelectorAll(".imagen_rescatado");
+
+for(let i=0;i<imagenes.length;i++){
+    imagenes[i].addEventListener("mouseover",()=>{
+    textos[i].style.animation = "textoRescatadoON 0.5s forwards"
+    textos[i].style.display = "block"
+})
+}
+
+for(let i=0;i<imagenes.length;i++){
+    imagenes[i].addEventListener("mouseout",()=>{
+    textos[i].style.display = "none"
+})
+}
